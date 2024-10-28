1752af6d3680c57592a5d527b0f9e37c2d6c51e0
KassidyJB <jeanbartkassidy51@gmail.com>
2024-10-28 19:01:08 -0400

    Update problem 3.py

--- problem 3.py
+++ problem 3.py
@@ -2,8 +2,8 @@ import random
 
 def greatest_num():
   num = 20
-  rand_list = []
-  max_num = max(rand_list)
+  rand_list = [1,20]
+  #max_num = max(rand_list)
   for i in range(num):
     rand_list.append(random.randint(2,20)
     print(rand_list)
