ea7600b99fbd3041ca8d415cffa42e543dfb565d
KassidyJB <jeanbartkassidy51@gmail.com>
2024-10-28 18:52:41 -0400

    Update problem 3.py

--- problem 3.py
+++ problem 3.py
@@ -1,1 +1,11 @@
+import random 
 
+def greatest_num():
+  num = 20
+  rand_list = []
+  max_num = max(rand_list)
+  for i in range(num):
+    rand_list.append(random.randint(2,20)
+    print(rand_list)
+    print(max(rand_list)
+greatest_num()
